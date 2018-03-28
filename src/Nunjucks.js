a2b92a39515225cb8799991445b659d84b418204
Philippe Vinchon <pvinchon@gilt.com>
2018-03-28 16:44:23 +0100

    Removed Renderer, overkill for now

--- src/Nunjucks.js
+++ src/Nunjucks.js
@@ -3,3 +3,15 @@
 const nunjucks = require("nunjucks");
 
 exports.default = nunjucks;
+
+exports.renderImpl = function(obj) {
+    return function(method) {
+        return function(str) {
+            return function(context) {
+                return function () {
+                    return obj[method](str, context);
+                };
+            };
+        };
+    };
+};
