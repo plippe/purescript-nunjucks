bcc96cea1813fc847eb4d50f3b99e79f81b1c2ea
Philippe Vinchon <pvinchon@gilt.com>
2018-03-28 16:54:47 +0100

    Add template compile

--- test/Main.purs
+++ test/Main.purs
@@ -5,9 +5,11 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
-import Test.Nunjucks.Render as Render
+import Test.Nunjucks as Nunjucks
+import Test.Nunjucks.Template as Template
 
 main :: forall eff. Eff (exception :: EXCEPTION, console :: CONSOLE | eff) Unit
 main = do
-  _ <- Render.main
+  _ <- Nunjucks.main
+  _ <- Template.main
   log "You should add some tests."
